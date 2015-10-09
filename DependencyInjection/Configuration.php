f54f3a0c60e151131d38d5eb19e070f071a53013
Ener-Getick <energetick.guigui@gmail.com>
2015-10-09 20:44:50 +0200

    Add a copyright header

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the WorkerBundle package.
+ *
+ * (c) EXSyst
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace EXSyst\Bundle\WorkerBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
